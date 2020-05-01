91589defccc8f752dc22ab55afab28aa28b83b5c
Ben Best <ben.best@olmgroup.com>
2020-05-01 18:17:21 +0100

    Fix: Better ribbon.

--- src/components/ribbon.js
+++ src/components/ribbon.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Ribbon = ({ repo }) => {
   return (
     <>
-    
-      <a href={repo}><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149"  style={{position:'absolute'}} alt="Fork me on GitHub" data-recalc-dims="1" /></a>
+    <a class="github-fork-ribbon" href={repo} data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
     </>
   )
 };
