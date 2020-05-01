0c16f61875828879ed525c08cbd5299c3fe84377
Ben Best <ben.best@olmgroup.com>
2020-05-01 15:16:23 +0100

    Chore: fix directory structure.

---
+++ build/precache-manifest.133b50c0b1efc9fad752398bf569efee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf20055af50befeac40482679ba52ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a147f4b7306ad8904d08",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "355318f74f7e42b7b254",
+    "url": "/static/js/2.08af2f66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08af2f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a147f4b7306ad8904d08",
+    "url": "/static/js/main.a55c1504.chunk.js"
+  },
+  {
+    "revision": "413a6cb270a55f817510",
+    "url": "/static/js/runtime-main.83642827.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
