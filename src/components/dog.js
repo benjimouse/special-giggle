7c7ec9732d82f34145480b53906521586d6519af
Ben Best <ben.best@olmgroup.com>
2020-05-01 17:00:16 +0100

    Feat: Now nicely centered

--- src/components/dog.js
+++ src/components/dog.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Dog = ({ dog }) => {
   return (
     <div>
-      <center><h1>Random Dog!</h1></center>
+      <center><h1>Random Dog!</h1>
       <img src={dog.message} alt="test" />
-      <p>Source is <a href="https://dog.ceo/dog-api/" >https://dog.ceo/dog-api/</a></p>
+      <p>Source is <a href="https://dog.ceo/dog-api/" >https://dog.ceo/dog-api/</a></p></center>
     </div>
   )
 };
